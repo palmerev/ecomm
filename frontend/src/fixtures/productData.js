d3824d7c4f8d2298f213600d1c51ad6157f0f673
Evan Palmer <evjpalmer@gmail.com>
2016-07-17 12:32:45 -0700

    fix missing commas in fixtureData.js

--- frontend/src/fixtures/productData.js
+++ frontend/src/fixtures/productData.js
@@ -1,19 +1,19 @@
 const productData = [
   {
-    name: "Widget 2000"
-    description: "A top-quality, high-performance widget, for tackling your toughest widget tasks. Power supply and multi-widget USB cable included. Runs up to 2000 cycles per minute."
+    name: "Widget 2000",
+    description: "A top-quality, high-performance widget, for tackling your toughest widget tasks. Power supply and multi-widget USB cable included. Runs up to 2000 cycles per minute.",
     price: 750,
     numInStock: 24,
   },
   {
-    name: "Widget 1500"
-    description: "A solid everyday widget, great for all your widget needs. Power supply and multi-widget USB cable included."
+    name: "Widget 1500",
+    description: "A solid everyday widget, great for all your widget needs. Power supply and multi-widget USB cable included.",
     price: 500,
     numInStock: 24,
   },
   {
-    name: "Widget 1000"
-    description: "A lightweight, portable widget, great for travel and everyday use. Power supply and multi-widget USB cable included."
+    name: "Widget 1000",
+    description: "A lightweight, portable widget, great for travel and everyday use. Power supply and multi-widget USB cable included.",
     price: 250,
     numInStock: 24,
   },
