74db69677ab2b8bb463e660a2751a718c384098c
Evan Palmer <evjpalmer@gmail.com>
2016-07-18 12:13:51 -0700

    add slug, totalSold, and features properties to widgets

--- frontend/src/fixtures/productData.js
+++ frontend/src/fixtures/productData.js
@@ -1,21 +1,45 @@
 const productData = [
   {
+    slug: "widget-2000",
     name: "Widget 2000",
     description: "A top-quality, high-performance widget, for tackling your toughest widget tasks. Power supply and multi-widget USB cable included. Runs up to 2000 cycles per minute.",
     price: 750,
     numInStock: 24,
+    totalSold: 376,
+    features: [
+      "30-day warrantee",
+      "suitable for indoor and outdoor use",
+      "sleek, attractive design",
+      "fully compatible with other widgets models"
+    ]
   },
   {
+    slug: "widget-1500",
     name: "Widget 1500",
     description: "A solid everyday widget, great for all your widget needs. Power supply and multi-widget USB cable included.",
     price: 500,
     numInStock: 24,
+    totalSold: 376,
+    features: [
+      "30-day warrantee",
+      "suitable for indoor and outdoor use",
+      "sleek, attractive design",
+      "fully compatible with other widgets models"
+    ]
   },
   {
+    slug: "widget-1000",
     name: "Widget 1000",
     description: "A lightweight, portable widget, great for travel and everyday use. Power supply and multi-widget USB cable included.",
     price: 250,
     numInStock: 24,
+    totalSold: 376,
+    features: [
+      "30-day warrantee",
+      "suitable for indoor and outdoor use",
+      "sleek, attractive design",
+      "fully compatible with other widgets models"
+    ]
   },
 ]
 
